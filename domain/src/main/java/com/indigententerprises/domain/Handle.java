b7e2c653139f02740c84c674ca8cb3fc87394a0d
jonnie savell <jonniesavell@mac.com>
2017-07-14 02:04:08 -0700

    premature ejabstraction

---
+++ domain/src/main/java/com/indigententerprises/domain/Handle.java
@@ -0,0 +1,14 @@
+package com.indigententerprises.domain;
+
+/**
+ *
+ * @author jonniesavell
+ */
+public class Handle {
+
+    public final String identifier;
+
+    public Handle(final String identifier) {
+        this.identifier = identifier;
+    }
+}
---
+++ services/src/main/java/com/indigententerprises/services/SystemException.java
@@ -0,0 +1,12 @@
+package com.indigententerprises.services;
+
+/**
+ *
+ * @author jonniesavell
+ */
+public class SystemException extends Exception {
+
+    public SystemException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
