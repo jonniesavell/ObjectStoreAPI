5cd313fa9a00b2011585a4159a4f21416b452d67
jonnie savell <jonniesavell@mac.com>
2017-07-23 03:46:06 -0700

    added code/how to doc to the serialization test

--- components/src/test/java/com/indigententerprises/components/attributes/AttributeSerializationTest.java
+++ components/src/test/java/com/indigententerprises/components/attributes/AttributeSerializationTest.java
@@ -2,14 +2,19 @@ package com.indigententerprises.components.attributes;
 
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
+import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
+
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Collection;
 
 /**
  *
@@ -23,6 +28,7 @@ public class AttributeSerializationTest {
     @Test
     public void test() throws Exception {
 
+        // this is how you create the schema the first time
         Schema ratingSchema = SchemaBuilder.record("Rating")
                 .fields()
                 .name("userId").type().intType().noDefault()
@@ -31,6 +37,7 @@ public class AttributeSerializationTest {
                 .name("timeInSeconds").type().intType().noDefault()
                 .endRecord();
 
+        // this is how you create a record without code generation
         GenericRecordBuilder recordBuilder =
                 new GenericRecordBuilder(ratingSchema)
                 .set("userId", 1)
@@ -39,7 +46,9 @@ public class AttributeSerializationTest {
                 .set("timeInSeconds", 1000);
 
         GenericRecord record = recordBuilder.build();
-        File file = new File("users.avro");
+
+        // this is how you persist both the schema and a record
+        File file = new File("movies.avro");
         DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(ratingSchema);
         DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter);
 
@@ -48,6 +57,35 @@ public class AttributeSerializationTest {
             dataFileWriter.append(record);
         } finally {
             dataFileWriter.close();
+        }
+
+        // this is how you read the file
+        final DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
+        final DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(file, datumReader);
+
+        // this is how you read the schema out of the file
+        final Schema schema = dataFileReader.getSchema();
+
+        // this is how you retrieve the fields from the schema
+        final Collection<Schema.Field> schemaFields = schema.getFields();
+
+        for (final Schema.Field schemaField : schemaFields) {
+
+            // these are the properties you'll need to interrogate
+            System.out.println("name        : " + schemaField.name());
+            System.out.println("schema-type : " + schemaField.schema().getType());
+            System.out.println("position    : " + schemaField.pos());
+        }
+
+        try {
+            GenericRecord movieData = null;
+
+            while (dataFileReader.hasNext()) {
+                movieData = dataFileReader.next(movieData);
+                System.out.println(movieData);
+            }
+        } finally {
+            dataFileReader.close();
         }
     }
 }
