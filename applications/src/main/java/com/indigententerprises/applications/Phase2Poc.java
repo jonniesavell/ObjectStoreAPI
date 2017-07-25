60ccecfea8f7d900a937f27d46e299dbcd6e2ec6
jonnie savell <jonniesavell@mac.com>
2017-07-24 23:28:43 -0700

    (1) added method storeObjectAndMetaData to the higher layer service (2) added a new POC application in order to demo the higher layer service

---
+++ applications/src/main/java/com/indigententerprises/applications/Phase2Poc.java
@@ -0,0 +1,124 @@
+package com.indigententerprises.applications;
+
+import com.indigententerprises.components.FileInvestigativeServiceImplementation;
+import com.indigententerprises.components.MetaDataServiceImplementation;
+import com.indigententerprises.components.ObjectService;
+import com.indigententerprises.components.ObjectServiceImplementation;
+import com.indigententerprises.components.TrivialStreamTransferService;
+
+import com.indigententerprises.services.FileInvestigativeService;
+import com.indigententerprises.services.StreamTransferService;
+import com.indigententerprises.services.SystemException;
+
+import com.indigententerprises.domain.FileData;
+import com.indigententerprises.domain.FileMetaData;
+import com.indigententerprises.domain.Handle;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+public class Phase2Poc {
+
+    public static void main(final String [] args) throws SystemException {
+
+        if (args.length != 3) {
+            throw new RuntimeException("usage: <exec> " + Phase2Poc.class.getName() + " <AWS bucket-name> <directory> <file>");
+        } else {
+            final String directoryName = args[1];
+            final String fileName = args[2];
+            final File directory = new File(directoryName);
+            final File file = new File(directory, fileName);
+
+            if (!file.canRead()) {
+                throw new RuntimeException("file " + file.getName() + " is not accessible");
+            } else {
+                final String requestedBucketName = args[0];
+
+                try {
+                    final EnvironmentVariableCredentialsProvider credentialsProvider =
+                            new EnvironmentVariableCredentialsProvider();
+                    final AmazonS3Client s3Client = (AmazonS3Client)
+                            AmazonS3ClientBuilder.standard().withCredentials(credentialsProvider).build();
+                    final AWSCredentials credentials =
+                            credentialsProvider.getCredentials();
+
+                    System.out.println("credentials: (" + credentials.getAWSAccessKeyId() + ", " + credentials.getAWSSecretKey() + ")");
+
+                    final boolean bucketExists =
+                            s3Client.doesBucketExist(requestedBucketName);
+                    s3Client.shutdown();
+
+                    if (!bucketExists) {
+                        throw new RuntimeException("bucket " + requestedBucketName + " not found");
+                    } else {
+                        final String targetBucketName = requestedBucketName;
+                        final FileInvestigativeService fileInvestigativeService =
+                                new FileInvestigativeServiceImplementation();
+                        final FileData fileData = fileInvestigativeService.investigate(file);
+                        final StreamTransferService streamTransferService = new TrivialStreamTransferService();
+                        final ObjectServiceImplementation primitiveObjectService =
+                                new ObjectServiceImplementation(
+                                        credentialsProvider,
+                                        targetBucketName,
+                                        streamTransferService
+                                );
+                        final MetaDataServiceImplementation primitiveMetaDataService =
+                                new MetaDataServiceImplementation();
+                        final ObjectService objectService =
+                                new ObjectService(primitiveObjectService, primitiveMetaDataService);
+                        final Handle handle;
+
+                        // begin write to S3
+                        handle = objectService.storeObjectAndMetaData(
+                                fileData.getInputStream(),
+                                (int) fileData.getFileMetaData().getSize(),
+                                new HashMap<>());
+                        // end
+
+                        final FileOutputStream fileOutputStream = new FileOutputStream(file, false);
+
+                        // truncate the local file
+                        try {
+                            FileChannel outChannel = fileOutputStream.getChannel();
+
+                            try {
+                                outChannel.truncate(0);
+                            } finally {
+                                outChannel.close();
+                            }
+                        } finally {
+                            fileOutputStream.close();
+                        }
+
+                        final FileOutputStream fileOutputStream2 = new FileOutputStream(file);
+
+                        try {
+                            // begin read from S3
+                            Map<String, Object> metadata =
+                                    objectService.retrieveObjectAndMetaData(handle, fileOutputStream2);
+                            // end
+
+                            System.out.println(metadata);
+                        } finally {
+                            fileOutputStream2.close();
+                        }
+                    }
+                } catch(IOException e){
+                    throw new SystemException("", e);
+                } catch(NoSuchElementException e){
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
--- components/src/main/java/com/indigententerprises/components/ObjectService.java
+++ components/src/main/java/com/indigententerprises/components/ObjectService.java
@@ -7,9 +7,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 /**
  *
@@ -94,6 +96,48 @@ public class ObjectService {
                     result =
                             this.primitiveMetaDataService.deserializeMetaData(fileInputStream);
                     this.primitiveObjectService.retrieveObject(handle.identifier, outputStream);
+
+                    return result;
+                } finally {
+                    fileInputStream.close();
+                }
+            } finally {
+                if (! file.delete()) {
+                    file.deleteOnExit();
+                }
+            }
+        } catch (IOException e) {
+            throw new SystemException("", e);
+        }
+    }
+
+    public Handle storeObjectAndMetaData(
+            final InputStream inputStream,
+            final int fileSize,
+            final Map<String, Object> metadata)
+            throws SystemException {
+        final UUID uuid = UUID.randomUUID();
+        final Handle result = new Handle(uuid.toString());
+
+        this.primitiveObjectService.persistObject(result.identifier, fileSize, inputStream);
+
+        try {
+            final File file = File.createTempFile("temp", ".tmp");
+
+            try {
+                final FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+                try {
+                    this.primitiveMetaDataService.serializeMetaData(fileOutputStream, metadata);
+                } finally {
+                    fileOutputStream.close();
+                }
+
+                final FileInputStream fileInputStream = new FileInputStream(file);
+
+                try {
+                    final String identifier = constructMetaDataIdentifier(result.identifier);
+                    this.primitiveObjectService.persistObject(identifier, (int) file.length(), fileInputStream);
 
                     return result;
                 } finally {
