ddd5b7da79618f816d6955fa7255337e12fa4ce3
jonnie savell <jonniesavell@mac.com>
2017-07-24 17:54:56 -0700

    (1) introduced checked exceptions: i am still unable to throw meaningful exceptions to the client because AWS does not throw meaningful exceptions to me, however. (2) added functionality to the ObjectService. TODO: ObjectService persistence method

--- components/src/main/java/com/indigententerprises/components/ObjectService.java
+++ components/src/main/java/com/indigententerprises/components/ObjectService.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -39,18 +39,18 @@ public class ObjectService {
                 final FileOutputStream fileOutputStream = new FileOutputStream(file);
 
                 try {
-                    final String identifier = handle.identifier + SUFFIX;
+                    final String identifier = constructMetaDataIdentifier(handle.identifier);
                     this.primitiveObjectService.retrieveObject(identifier, fileOutputStream);
                 } finally {
                     fileOutputStream.close();
+                }
 
-                    final FileInputStream fileInputStream = new FileInputStream(file);
+                final FileInputStream fileInputStream = new FileInputStream(file);
 
-                    try {
-                        return this.primitiveMetaDataService.deserializeMetaData(fileInputStream);
-                    } finally {
-                        fileInputStream.close();
-                    }
+                try {
+                    return this.primitiveMetaDataService.deserializeMetaData(fileInputStream);
+                } finally {
+                    fileInputStream.close();
                 }
             } finally {
                 if (! file.delete()) {
@@ -64,7 +64,52 @@ public class ObjectService {
 
     public void retrieveObject(
             final Handle handle,
-            final InputStream inputStream)
+            final OutputStream outputStream)
             throws NoSuchElementException, SystemException {
+
+        this.primitiveObjectService.retrieveObject(handle.identifier, outputStream);
+    }
+
+    public Map<String, Object> retrieveObjectAndMetaData(
+            final Handle handle,
+            final OutputStream outputStream)
+            throws NoSuchElementException, SystemException {
+        try {
+            final File file = File.createTempFile("temp", ".tmp");
+
+            try {
+                final FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+                try {
+                    final String identifier = constructMetaDataIdentifier(handle.identifier);
+                    this.primitiveObjectService.retrieveObject(identifier, fileOutputStream);
+                } finally {
+                    fileOutputStream.close();
+                }
+
+                final Map<String, Object> result;
+                final FileInputStream fileInputStream = new FileInputStream(file);
+
+                try {
+                    result =
+                            this.primitiveMetaDataService.deserializeMetaData(fileInputStream);
+                    this.primitiveObjectService.retrieveObject(handle.identifier, outputStream);
+
+                    return result;
+                } finally {
+                    fileInputStream.close();
+                }
+            } finally {
+                if (! file.delete()) {
+                    file.deleteOnExit();
+                }
+            }
+        } catch (IOException e) {
+            throw new SystemException("", e);
+        }
+    }
+
+    private String constructMetaDataIdentifier(final String identifier) {
+        return identifier + SUFFIX;
     }
 }
--- components/src/main/java/com/indigententerprises/components/ObjectServiceImplementation.java
+++ components/src/main/java/com/indigententerprises/components/ObjectServiceImplementation.java
@@ -1,5 +1,8 @@
 package com.indigententerprises.components;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.AmazonClientException;
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 
@@ -13,10 +16,12 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 import com.indigententerprises.services.StreamTransferService;
+import com.indigententerprises.services.SystemException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.NoSuchElementException;
 
 public class ObjectServiceImplementation {
 
@@ -28,53 +33,11 @@ public class ObjectServiceImplementation {
     public ObjectServiceImplementation(
             final AWSCredentialsProvider credentialsProvider,
             final String targetBucketName,
-            final StreamTransferService streamTransferService) {
+            final StreamTransferService streamTransferService) throws SystemException {
 
         this.credentialsProvider = credentialsProvider;
         this.targetBucketName = targetBucketName;
         this.streamTransferService = streamTransferService;
-
-        // down-casting is BAAAD
-        final AmazonS3Client s3Client = (AmazonS3Client)
-            AmazonS3ClientBuilder.standard().withCredentials(this.credentialsProvider).build();
-
-        try {
-            if (!s3Client.doesBucketExist(this.targetBucketName)) {
-                throw new RuntimeException("bucket " + this.targetBucketName + " not found");
-            } else {
-                final AWSCredentials credentials = credentialsProvider.getCredentials();
-                System.out.println("credentials: (" + credentials.getAWSAccessKeyId() + ", " + credentials.getAWSSecretKey() + ")");
-            }
-        } finally {
-            s3Client.shutdown();
-        }
-    }
-
-    public synchronized void persistObject(
-            final String id,
-            final int size,
-            final InputStream sourceInputStream) {
-
-        // down-casting is BAAAD
-        final AmazonS3Client s3Client = (AmazonS3Client)
-                AmazonS3ClientBuilder.standard().withCredentials(this.credentialsProvider).build();
-
-        try {
-            final ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(size);
-
-            final PutObjectRequest request =
-                    new PutObjectRequest(targetBucketName, id, sourceInputStream, metadata);
-
-            s3Client.putObject(request);
-        } finally {
-            s3Client.shutdown();
-        }
-    }
-
-    public synchronized void retrieveObject(
-            final String id,
-            final OutputStream outputStream) {
 
         try {
             // down-casting is BAAAD
@@ -82,25 +45,90 @@ public class ObjectServiceImplementation {
                     AmazonS3ClientBuilder.standard().withCredentials(this.credentialsProvider).build();
 
             try {
-                final GetObjectRequest getObjectRequest = new GetObjectRequest(this.targetBucketName, id);
-                final S3Object s3Object = s3Client.getObject(getObjectRequest);
+                if (!s3Client.doesBucketExist(this.targetBucketName)) {
+                    throw new RuntimeException("bucket " + this.targetBucketName + " not found");
+                } else {
+                    final AWSCredentials credentials = credentialsProvider.getCredentials();
+                    System.out.println("credentials: (" + credentials.getAWSAccessKeyId() + ", " + credentials.getAWSSecretKey() + ")");
+                }
+            } finally {
+                s3Client.shutdown();
+            }
+        } catch (AmazonServiceException e) {
+            throw new SystemException("", e);
+        } catch (AmazonClientException e) {
+            throw new SystemException("", e);
+        }
+    }
 
-                try {
-                    final S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
+    public synchronized void persistObject(
+            final String id,
+            final int size,
+            final InputStream sourceInputStream) throws SystemException {
+
+        try {
+            // down-casting is BAAAD
+            final AmazonS3Client s3Client = (AmazonS3Client)
+                    AmazonS3ClientBuilder.standard().withCredentials(this.credentialsProvider).build();
+
+            try {
+                final ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentLength(size);
+
+                final PutObjectRequest request =
+                        new PutObjectRequest(targetBucketName, id, sourceInputStream, metadata);
+
+                s3Client.putObject(request);
+            } finally {
+                s3Client.shutdown();
+            }
+        } catch (AmazonServiceException e) {
+            throw new SystemException("", e);
+        } catch (AmazonClientException e) {
+            throw new SystemException("", e);
+        }
+    }
+
+    public synchronized void retrieveObject(
+            final String id,
+            final OutputStream outputStream) throws NoSuchElementException, SystemException {
+
+        try {
+            // down-casting is BAAAD
+            final AmazonS3Client s3Client = (AmazonS3Client)
+                    AmazonS3ClientBuilder.standard().withCredentials(this.credentialsProvider).build();
+
+            try {
+                if (! s3Client.doesObjectExist(this.targetBucketName, id)) {
+                    throw new NoSuchElementException();
+                } else {
+                    final GetObjectRequest getObjectRequest =
+                            new GetObjectRequest(this.targetBucketName, id);
+                    final S3Object s3Object =
+                            s3Client.getObject(getObjectRequest);
 
                     try {
-                        streamTransferService.transferStreamData(s3ObjectInputStream, outputStream);
+                        final S3ObjectInputStream s3ObjectInputStream =
+                                s3Object.getObjectContent();
+
+                        try {
+                            streamTransferService.transferStreamData(s3ObjectInputStream, outputStream);
+                        } finally {
+                            s3ObjectInputStream.close();
+                        }
                     } finally {
-                        s3ObjectInputStream.close();
+                        s3Object.close();
                     }
-                } finally {
-                    s3Object.close();
                 }
             } finally {
                 s3Client.shutdown();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new SystemException("", e);
+        } catch (AmazonServiceException e) {
+            throw new SystemException("", e);
+        } catch (AmazonClientException e) {
+            throw new SystemException("", e);
         }
     }
 }
